5a45ca034a2525be66e8ac3f4fd11233be97ea8b
Joe Quattrone <joequattrone5@gmail.com>
2019-06-05 14:41:18 -0400

    write test for update_likes controller method

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -19,8 +19,12 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
   test 'should update likes of given song' do
     exisiting_song_data = {"track"=>{"id"=>1, "artist_name"=>"Eminem", "track_id"=>1809819, "track_name"=>"Lose Yourself", "genre"=>"Alternative Rap", "likes"=>29, "created_at"=>"2019-05-07T19:28:38.690Z", "updated_at"=>"2019-06-04T14:44:56.307Z"}}
 
+    @song = Song.find_by(track_id: exisiting_song_data["track"]["track_id"])
+    likes = exisiting_song_data["track"]["likes"]
+    
     post update_likes_url, params: { song: exisiting_song_data}, as: :json
     assert_response 204
+    assert_equal likes + 1, @song.likes
   end
 
   # test "should create song" do
